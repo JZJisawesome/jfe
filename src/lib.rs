c4ec64c05caccba85182c168f414f771547bfe40
John Jekel <JZJisawesome123@gmail.com>
2022-12-24 22:34:40 -0500

    Implement the mandelbrot fractal

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,14 @@
 /* Imports */
 
 pub mod mandelbrot;
-mod numerical_traits;
+//mod numerical_traits;
 pub mod ini;
 //pub mod fractal_names;
-use std::any::Any;//Overhead of Any is minimal compared to slow speed of updating fractals
 use std::fmt::Debug;
 
-use std::ops::Index;
+//use std::ops::Index;
 
-use crate::numerical_traits::Integer;
+//use crate::numerical_traits::Integer;
 
 /* Constants */
 
@@ -90,147 +89,6 @@ pub trait RandomFractal: BaseFractal {
 pub trait FiniteSubdivisionFractal: BaseFractal {
     //TODO
 }
-/*
-pub trait AnyBaseFractal : Any + BaseFractal {}//Overhead of Any is minimal compared to slow speed of updating fractals
-
-//pub type Fractal = IterationCountFractal;
-//pub trait IterationCountFractal {//Fractals that provide iteration counts only
-pub trait BaseFractal {//Fractals that provide dots only (the bare minimum)
-    //Constructor
-    /*fn new(
-        iterations: usize,
-        x_dots: usize, y_dots: usize,
-        min_real: f64, max_real: f64,
-        min_imag: f64, max_imag: f64
-    ) -> Self;
-    */
-
-    //Getters
-    fn get_iterations(self: &Self) -> usize;//Meaning depends on the fractal
-    fn get_x_dots(self: &Self) -> usize;
-    fn get_y_dots(self: &Self) -> usize;
-    fn get_min_real(self: &Self) -> f64;
-    fn get_max_real(self: &Self) -> f64;
-    fn get_min_imag(self: &Self) -> f64;
-    fn get_max_imag(self: &Self) -> f64;
-
-    //Setters
-    fn set_iterations(self: &mut Self, max_iterations: usize);//Meaning depends on the fractal
-    fn set_x_dots(self: &mut Self, x_dots: usize);
-    fn set_y_dots(self: &mut Self, y_dots: usize);
-    fn set_min_real(self: &mut Self, min_real: f64);
-    fn set_max_real(self: &mut Self, max_real: f64);
-    fn set_min_imag(self: &mut Self, min_imag: f64);
-    fn set_max_imag(self: &mut Self, max_imag: f64);
-
-    //The meaning of what a dot is depends on the fractal
-
-    //Access Dots Storage
-    fn dots_ref(self: &Self) -> Option::<&[usize]>;//Returns None if update() wasn't called since the last change to arguments/since construction
-
-    //Update Dots
-    fn update_dots(self: &mut Self);
-}
-
-pub trait FractalKeepingLastValue: BaseFractal {
-    //TODO this also keeps the last computed value of Z for use with certain colouring algorithms
-}
-
-/*pub enum NewOldFractal {
-    Double(dyn FractalType<f64>),
-    Float(dyn FractalType<f32>)
-}
-*/
-
-pub trait OldFractalType<FloatType> {
-    //Constructor
-    fn new(
-        iterations: usize,
-        x_dots: usize, y_dots: usize,
-        min_real: FloatType, max_real: FloatType,
-        min_imag: FloatType, max_imag: FloatType
-    ) -> Self;
-
-    //Getters
-    fn get_iterations(self: &Self) -> usize;//Meaning depends on the fractal
-    fn get_x_dots(self: &Self) -> usize;
-    fn get_y_dots(self: &Self) -> usize;
-    fn get_min_real(self: &Self) -> FloatType;
-    fn get_max_real(self: &Self) -> FloatType;
-    fn get_min_imag(self: &Self) -> FloatType;
-    fn get_max_imag(self: &Self) -> FloatType;
-
-    //Setters
-    fn set_iterations(self: &mut Self, max_iterations: usize);//Meaning depends on the fractal
-    fn set_x_dots(self: &mut Self, x_dots: usize);
-    fn set_y_dots(self: &mut Self, y_dots: usize);
-    fn set_min_real(self: &mut Self, min_real: FloatType);
-    fn set_max_real(self: &mut Self, max_real: FloatType);
-    fn set_min_imag(self: &mut Self, min_imag: FloatType);
-    fn set_max_imag(self: &mut Self, max_imag: FloatType);
-
-    //The meaning of what a dot is depends on the fractal
-
-    //Access Dots Storage
-    fn dots_ref(self: &Self) -> Option::<&[usize]>;//Returns None if update() wasn't called since the last change to arguments/since construction
-
-    //Update Dots
-    fn update_dots(self: &mut Self);
-}
-
-//TODO create a CustomFunctionFractal that allows the user to define custom (but slower) fractals
-
-pub trait OldFractal {//Fractals that provide iteration counts only
-    type FractalDotType: Integer;
-    type FractalFloatType;
-
-    //Constructor
-    fn new(
-        iterations: usize,
-        x_dots: usize, y_dots: usize,
-        min_real: Self::FractalFloatType, max_real: Self::FractalFloatType,
-        min_imag: Self::FractalFloatType, max_imag: Self::FractalFloatType
-    ) -> Self;
-
-    //Getters
-    fn get_iterations(self: &Self) -> usize;//Meaning depends on the fractal
-    fn get_x_dots(self: &Self) -> usize;
-    fn get_y_dots(self: &Self) -> usize;
-    fn get_min_real(self: &Self) -> Self::FractalFloatType;
-    fn get_max_real(self: &Self) -> Self::FractalFloatType;
-    fn get_min_imag(self: &Self) -> Self::FractalFloatType;
-    fn get_max_imag(self: &Self) -> Self::FractalFloatType;
-
-    //Setters
-    fn set_iterations(self: &mut Self, max_iterations: usize);//Meaning depends on the fractal
-    fn set_x_dots(self: &mut Self, x_dots: usize);
-    fn set_y_dots(self: &mut Self, y_dots: usize);
-    fn set_min_real(self: &mut Self, min_real: Self::FractalFloatType);
-    fn set_max_real(self: &mut Self, max_real: Self::FractalFloatType);
-    fn set_min_imag(self: &mut Self, min_imag: Self::FractalFloatType);
-    fn set_max_imag(self: &mut Self, max_imag: Self::FractalFloatType);
-
-    //The meaning of what a dot is depends on the fractal
-
-    //Access Dots Storage
-    fn dots_ref(self: &Self) -> Option::<&[Self::FractalDotType]>;//Returns None if update() wasn't called since the last change to arguments/since construction
-
-    //Update Dots
-    fn update_dots(self: &mut Self);
-}
-
-/*impl<T> Index<usize> for T where T: Fractal {
-    type Output = usize;
-
-    fn index(self: &Self, index: usize) -> &Self::Output {
-        return self.iterations_ref().index(index);
-    }
-}
-*/
-*/
-//pub trait FractalKeepingLastValue: Fractal {
-    //TODO this also keeps the last computed value of Z for use with certain colouring algorithms
-//}
 
 /* Associated Functions and Methods */
 
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -7,6 +7,8 @@
 
 /* Imports */
 
+use std::ops::IndexMut;
+
 use crate::BaseFractal;
 use crate::EscapeTimeFractal;
 
@@ -56,7 +58,7 @@ impl Mandelbrot {
         let mut new_iterations_vec = Vec::<usize>::with_capacity(x_samples * y_samples);
         new_iterations_vec.resize(x_samples * y_samples, 0);
 
-        let mut new_mandelbrot = Mandelbrot {
+        return Mandelbrot {
             max_iterations: max_iterations,
             x_samples: x_samples,
             y_samples: y_samples,
@@ -69,7 +71,38 @@ impl Mandelbrot {
             iterations: new_iterations_vec,
             update_pending: true
         };
-        return new_mandelbrot;
+    }
+
+    fn mandelbrot_iterations(self: &Self, c_real: f64, c_imag: f64) -> usize {//Returns MAX_ITERATIONS if it is bounded
+        //println!("mandelbrot iteration with params: {} {}", c_real, c_imag);
+        let diverge_threshold: f64 = 2.0;//TODO make this flexible?
+
+        //z_0 = 0
+        let mut z_real: f64 = 0.0;
+        let mut z_imag: f64 = 0.0;
+
+        //We exit the loop in two cases: if we reach MAX_ITERATIONS (meaning we assume the c value produces a bounded series)
+        //or the modulus of the complex number exceeds the diverge_threshold (meaning the c value produces an unbounded series)
+        let mut i: usize = 0;
+        while (i < self.max_iterations) && (((z_real * z_real) + (z_imag * z_imag)) < (diverge_threshold * diverge_threshold)) {
+            //println!("iteration {} starts: z_real {}, z_imag {}", i, z_real, z_imag);
+            //z_(n+1) = z_n^2 + c
+            let next_z_real = (z_real * z_real) - (z_imag * z_imag) + c_real;
+            let next_z_imag = (2.0 * z_real * z_imag) + c_imag;
+            z_real = next_z_real;
+            z_imag = next_z_imag;
+            //println!("iteration {} ends: z_real {}, z_imag {}", i, z_real, z_imag);
+            i += 1;
+        }
+        //println!("mandelbrot ends returning {}", i);
+        return i;
+    }
+
+    #[inline(always)]
+    fn at(self: &mut Self, x: usize, y: usize) -> &mut usize {//unchecked for speed in release builds
+        debug_assert!(x < self.x_samples);
+        debug_assert!(y < self.y_samples);
+        return self.iterations.index_mut(x + (y * self.x_samples));
     }
 }
 
@@ -86,8 +119,21 @@ impl BaseFractal for Mandelbrot {
 
     //Update Samples
     fn update(self: &mut Self) {
+        let real_length: f64 = self.max_real - self.min_real;
+        let real_step_amount: f64 = real_length / (self.x_samples as f64);
+        let imag_length: f64 = self.max_imag - self.min_imag;
+        let imag_step_amount: f64 = imag_length / (self.y_samples as f64);
+
+        let mut c_real: f64 = self.min_real;
+        for x in 0..self.x_samples {
+            let mut c_imag: f64 = self.min_imag;
+            for y in 0..self.y_samples {
+                *self.at(x, y) = self.mandelbrot_iterations(c_real, c_imag);
+                c_imag += imag_step_amount;
+            }
+            c_real += real_step_amount;
+        }
         self.update_pending = false;
-        todo!();
     }
 }
 
@@ -171,116 +217,9 @@ impl EscapeTimeFractal for Mandelbrot {
             return None;
         }
 
-        todo!();
+        return Some(&self.iterations);
     }
 }
-
-/*pub struct AccurateMandelbrotFractal {
-    x_pixels: usize,
-    y_pixels: usize,
-    min_real: f64,
-    min_imag: f64,
-    max_real: f64,
-    max_imag: f64,
-    iterations: Vec::<usize>//For cheap resizing in case the user changes x_pixels or y_pixels
-}
-
-impl AccurateMandelbrotFractal {
-    fn get_max_threads(self: &Self) -> usize {
-        todo!();
-    }
-
-    fn set_max_threads(self: &mut Self, max_threads: usize) {
-        todo!();
-    }
-}
-
-impl Fractal for AccurateMandelbrotFractal {
-    type FractalFloatType = f64;//High precision, but slow...
-
-    //Constructor
-    fn new(
-        iterations: usize,
-        x_pixels: usize, y_pixels: usize,
-        min_real: Self::FractalFloatType, max_real: Self::FractalFloatType,
-        min_imag: Self::FractalFloatType, max_imag: Self::FractalFloatType
-    ) -> Self {
-        todo!();
-    }
-
-    //Getters
-    fn get_iterations(self: &Self) -> usize {
-        todo!();
-    }
-
-    fn get_x_pixels(self: &Self) -> usize {
-        todo!();
-    }
-
-    fn get_y_pixels(self: &Self) -> usize {
-        todo!();
-    }
-
-    fn get_min_real(self: &Self) -> Self::FractalFloatType {
-        todo!();
-    }
-
-    fn get_max_real(self: &Self) -> Self::FractalFloatType {
-        todo!();
-    }
-
-    fn get_min_imag(self: &Self) -> Self::FractalFloatType {
-        todo!();
-    }
-
-    fn get_max_imag(self: &Self) -> Self::FractalFloatType {
-        todo!();
-    }
-
-
-    //Setters
-    fn set_iterations(self: &mut Self, max_iterations: usize) {
-        todo!();
-    }
-
-    fn set_x_pixels(self: &mut Self, x_pixels: usize) {
-        todo!();
-    }
-
-    fn set_y_pixels(self: &mut Self, y_pixels: usize) {
-        todo!();
-    }
-
-    fn set_min_real(self: &mut Self, min_real: Self::FractalFloatType) {
-        todo!();
-    }
-
-    fn set_max_real(self: &mut Self, max_real: Self::FractalFloatType) {
-        todo!();
-    }
-
-    fn set_min_imag(self: &mut Self, min_imag: Self::FractalFloatType) {
-        todo!();
-    }
-
-    fn set_max_imag(self: &mut Self, max_imag: Self::FractalFloatType) {
-        todo!();
-    }
-
-
-    //Access Iteration Storage
-    fn iterations_ref(self: &Self) -> Option::<&[usize]> {//Returns None if update_iterations() wasn't called since the last change to arguments/since construction
-        todo!();
-    }
-
-    //Update Iterations
-    fn update_iterations(self: &mut Self) {
-        todo!();
-    }
-
-}
-
-*/
 
 /* Associated Functions and Methods */
 
