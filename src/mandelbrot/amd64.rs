e338318360d72aae39a9b8beddd139c934d8014f
John Zacarias Jekel <JZJisawesome123@gmail.com>
2022-12-29 18:40:56 -0500

    Remove uninterleaved SIMD implementations

--- src/mandelbrot/amd64.rs
+++ src/mandelbrot/amd64.rs
@@ -71,86 +71,6 @@ impl Mandelbrot {
     #[cfg(target_arch = "x86_64")]
     #[inline]//But this is okay
     #[target_feature(enable = "avx2")]
-    unsafe fn old_mandelbrot_iterations_avx2(self: &Self, c_real_f: x86_64::__m256d, c_imag_f: x86_64::__m256d) -> x86_64::__m256i {
-        let diverge_threshold: f64 = 2.0;//TODO make this flexible?
-        let diverge_threshold_squared_f = x86_64::_mm256_set1_pd(diverge_threshold * diverge_threshold);
-        let two_f = x86_64::_mm256_set1_pd(2.0);
-
-        let mut result_i = x86_64::_mm256_set1_epi64x(0);
-
-        let mut incrementor_i_as_f = x86_64::_mm256_castsi256_pd(x86_64::_mm256_set1_epi64x(1));//We increment the result counter until we go past the diverge_threshold, then we never do again
-
-        let mut z_real_f = x86_64::_mm256_set1_pd(0.0);
-        let mut z_imag_f = x86_64::_mm256_set1_pd(0.0);
-
-        for _ in 0..self.max_iterations {
-            //Calculate some values that are used below
-            let z_real_squared_f = x86_64::_mm256_mul_pd(z_real_f, z_real_f);
-            let z_imag_squared_f = x86_64::_mm256_mul_pd(z_imag_f, z_imag_f);
-
-            //Check if the modulus of each z < the diverge value (aka that they haven't diverged)
-            //We do this faster by doing (z_real * z_real) + (z_imag * z_imag) < (2 * 2)
-            let squared_sum_f = x86_64::_mm256_add_pd(z_real_squared_f, z_imag_squared_f);
-            let compare_i_as_f = x86_64::_mm256_cmp_pd(squared_sum_f, diverge_threshold_squared_f, x86_64::_CMP_LT_OQ);
-
-            //Get next entries (For each complex number z, z_(n+1) = z_n^2 + c)
-            //We do this before the diverge check below, instead of after like we used to because it better masks the latency of
-            //A) The comparisons performed above to the check below
-            //B) The calculation of the next z_imag and z_real to the squaring at the start of the next iteration of the loop
-            //Also this dosn't compromise the latency of the squaring at the start of the loop to this since we still have the compare above in-between
-            let temp_z_real_f = x86_64::_mm256_add_pd(x86_64::_mm256_sub_pd(z_real_squared_f, z_imag_squared_f), c_real_f);
-            z_imag_f = x86_64::_mm256_add_pd(c_imag_f, x86_64::_mm256_mul_pd(two_f, x86_64::_mm256_mul_pd(z_real_f, z_imag_f)));
-            z_real_f = temp_z_real_f;
-
-            //If both complex numbers have diverged (entire vector is 0), return
-            if x86_64::_mm256_movemask_pd(compare_i_as_f) == 0 {
-                break;
-            }
-
-            //Increment the corresponding count only if we haven't converged yet
-            incrementor_i_as_f = x86_64::_mm256_and_pd(compare_i_as_f, incrementor_i_as_f);
-            let incrementor_i = x86_64::_mm256_castpd_si256(incrementor_i_as_f);//If a number hasn't converged, we will increment it's count
-            result_i = x86_64::_mm256_add_epi64(result_i, incrementor_i);//Requires AVX2
-        }
-
-        return result_i;
-    }
-
-    #[cfg(target_arch = "x86_64")]
-    #[target_feature(enable = "avx2")]
-    unsafe fn old_update_avx2(self: &mut Self) {
-        debug_assert!((self.x_samples & 0b11) == 0);//TODO overcome this limitation
-
-        //TODO actually use avx here
-
-        let real_length: f64 = self.max_real - self.min_real;
-        let real_step_amount: f64 = real_length / (self.x_samples as f64);
-        let imag_length: f64 = self.max_imag - self.min_imag;
-        let imag_step_amount: f64 = imag_length / (self.y_samples as f64);
-
-        let iterations_pointer = self.iterations.as_mut_ptr();
-
-        let real_step_amount_vector = x86_64::_mm256_set1_pd(real_step_amount * 4.0);//x4 since we process four at a time
-        let imag_step_amount_vector = x86_64::_mm256_set1_pd(imag_step_amount);
-
-        let mut c_real = x86_64::_mm256_set_pd(self.min_real + (real_step_amount * 3.0), self.min_real + (real_step_amount * 2.0), self.min_real + real_step_amount, self.min_real);
-        for x in (0..self.x_samples).step_by(4) {
-            let mut c_imag = x86_64::_mm256_set1_pd(self.min_imag);
-            for y in 0..self.y_samples {
-                let result = self.old_mandelbrot_iterations_avx2(c_real, c_imag);
-                let pointer = iterations_pointer.offset((x + (y * self.x_samples)) as isize) as *mut x86_64::__m256i;
-                x86_64::_mm256_storeu_si256(pointer, result);
-                c_imag = x86_64::_mm256_add_pd(c_imag, imag_step_amount_vector);
-            }
-            c_real = x86_64::_mm256_add_pd(c_real, real_step_amount_vector);
-        }
-
-        self.update_pending = false;
-    }
-
-    #[cfg(target_arch = "x86_64")]
-    #[inline]//But this is okay
-    #[target_feature(enable = "avx2")]
     unsafe fn mandelbrot_iterations_avx2(self: &Self, c_real_f: [x86_64::__m256d; 2], c_imag_f: [x86_64::__m256d; 2]) -> [x86_64::__m256i; 2] {
         let diverge_threshold: f64 = 2.0;//TODO make this flexible?
         let diverge_threshold_squared_f = x86_64::_mm256_set1_pd(diverge_threshold * diverge_threshold);
@@ -223,92 +143,6 @@ impl Mandelbrot {
             }
             c_real = [x86_64::_mm256_add_pd(c_real[0], real_step_amount_vector), x86_64::_mm256_add_pd(c_real[1], real_step_amount_vector)];
         }
-        self.update_pending = false;
-    }
-
-    #[cfg(target_arch = "x86_64")]
-    #[inline]//But this is okay
-    #[target_feature(enable = "avx")]
-    unsafe fn old_mandelbrot_iterations_avx(self: &Self, c_real_f: x86_64::__m256d, c_imag_f: x86_64::__m256d) -> x86_64::__m256i {
-        let diverge_threshold: f64 = 2.0;//TODO make this flexible?
-        let diverge_threshold_squared_f = x86_64::_mm256_set1_pd(diverge_threshold * diverge_threshold);
-        let two_f = x86_64::_mm256_set1_pd(2.0);
-
-        let mut result_i = x86_64::_mm256_set1_epi64x(0);
-
-        let mut incrementor_i_as_f = x86_64::_mm256_castsi256_pd(x86_64::_mm256_set1_epi64x(1));//We increment the result counter until we go past the diverge_threshold, then we never do again
-
-        let mut z_real_f = x86_64::_mm256_set1_pd(0.0);
-        let mut z_imag_f = x86_64::_mm256_set1_pd(0.0);
-
-        for _ in 0..self.max_iterations {
-            //Calculate some values that are used below
-            let z_real_squared_f = x86_64::_mm256_mul_pd(z_real_f, z_real_f);
-            let z_imag_squared_f = x86_64::_mm256_mul_pd(z_imag_f, z_imag_f);
-
-            //Check if the modulus of each z < the diverge value (aka that they haven't diverged)
-            //We do this faster by doing (z_real * z_real) + (z_imag * z_imag) < (2 * 2)
-            let squared_sum_f = x86_64::_mm256_add_pd(z_real_squared_f, z_imag_squared_f);
-            let compare_i_as_f = x86_64::_mm256_cmp_pd(squared_sum_f, diverge_threshold_squared_f, x86_64::_CMP_LT_OQ);
-
-            //Get next entries (For each complex number z, z_(n+1) = z_n^2 + c)
-            //We do this before the diverge check below, instead of after like we used to because it better masks the latency of
-            //A) The comparisons performed above to the check below
-            //B) The calculation of the next z_imag and z_real to the squaring at the start of the next iteration of the loop
-            //Also this dosn't compromise the latency of the squaring at the start of the loop to this since we still have the compare above in-between
-            let temp_z_real_f = x86_64::_mm256_add_pd(x86_64::_mm256_sub_pd(z_real_squared_f, z_imag_squared_f), c_real_f);
-            z_imag_f = x86_64::_mm256_add_pd(c_imag_f, x86_64::_mm256_mul_pd(two_f, x86_64::_mm256_mul_pd(z_real_f, z_imag_f)));
-            z_real_f = temp_z_real_f;
-
-            //If both complex numbers have diverged (entire vector is 0), return
-            if x86_64::_mm256_movemask_pd(compare_i_as_f) == 0 {
-                break;
-            }
-
-            //Increment the corresponding count only if we haven't converged yet
-            incrementor_i_as_f = x86_64::_mm256_and_pd(compare_i_as_f, incrementor_i_as_f);
-            let incrementor_i = x86_64::_mm256_castpd_si256(incrementor_i_as_f);//If a number hasn't converged, we will increment it's count
-            //Since we don't have AVX2, we must add the top and bottom separately
-            let lower_result_i = x86_64::_mm256_castsi256_si128(result_i);
-            let upper_result_i = x86_64::_mm256_extractf128_si256(result_i, 1);
-            let lower_incrementor_i = x86_64::_mm256_castsi256_si128(incrementor_i);
-            let upper_incrementor_i = x86_64::_mm256_extractf128_si256(incrementor_i, 1);
-            let lower_sum_i = x86_64::_mm_add_epi64(lower_result_i, lower_incrementor_i);
-            let upper_sum_i = x86_64::_mm_add_epi64(upper_result_i, upper_incrementor_i);
-            result_i = x86_64::_mm256_castsi128_si256(lower_sum_i);
-            result_i = x86_64::_mm256_insertf128_si256(result_i, upper_sum_i, 1);
-        }
-
-        return result_i;
-    }
-
-    #[cfg(target_arch = "x86_64")]
-    #[target_feature(enable = "avx")]
-    unsafe fn old_update_avx(self: &mut Self) {
-        debug_assert!((self.x_samples & 0b11) == 0);//TODO overcome this limitation
-
-        let real_length: f64 = self.max_real - self.min_real;
-        let real_step_amount: f64 = real_length / (self.x_samples as f64);
-        let imag_length: f64 = self.max_imag - self.min_imag;
-        let imag_step_amount: f64 = imag_length / (self.y_samples as f64);
-
-        let iterations_pointer = self.iterations.as_mut_ptr();
-
-        let real_step_amount_vector = x86_64::_mm256_set1_pd(real_step_amount * 4.0);//x4 since we process four at a time
-        let imag_step_amount_vector = x86_64::_mm256_set1_pd(imag_step_amount);
-
-        let mut c_real = x86_64::_mm256_set_pd(self.min_real + (real_step_amount * 3.0), self.min_real + (real_step_amount * 2.0), self.min_real + real_step_amount, self.min_real);
-        for x in (0..self.x_samples).step_by(4) {
-            let mut c_imag = x86_64::_mm256_set1_pd(self.min_imag);
-            for y in 0..self.y_samples {
-                let result = self.old_mandelbrot_iterations_avx(c_real, c_imag);
-                let pointer = iterations_pointer.offset((x + (y * self.x_samples)) as isize) as *mut x86_64::__m256i;
-                x86_64::_mm256_storeu_si256(pointer, result);
-                c_imag = x86_64::_mm256_add_pd(c_imag, imag_step_amount_vector);
-            }
-            c_real = x86_64::_mm256_add_pd(c_real, real_step_amount_vector);
-        }
-
         self.update_pending = false;
     }
 
@@ -651,26 +485,6 @@ mod benches {
 
     #[bench]
     #[cfg(target_arch = "x86_64")]
-    fn old_update_avx(b: &mut Bencher) {
-        if is_x86_feature_detected!("avx") {
-            let mandelbrot = Mandelbrot::new(
-                1024,
-                128,
-                128,
-                -2.3, 0.8,
-                -1.1, 1.1
-            );
-
-            b.iter(|| -> Mandelbrot {
-                let mut copy = mandelbrot.clone();
-                unsafe { copy.old_update_avx() };
-                return copy;
-            });
-        }
-    }
-
-    #[bench]
-    #[cfg(target_arch = "x86_64")]
     fn update_avx2(b: &mut Bencher) {
         if is_x86_feature_detected!("avx2") {
             let mandelbrot = Mandelbrot::new(
@@ -684,26 +498,6 @@ mod benches {
             b.iter(|| -> Mandelbrot {
                 let mut copy = mandelbrot.clone();
                 unsafe { copy.update_avx2() };
-                return copy;
-            });
-        }
-    }
-
-    #[bench]
-    #[cfg(target_arch = "x86_64")]
-    fn old_update_avx2(b: &mut Bencher) {
-        if is_x86_feature_detected!("avx2") {
-            let mandelbrot = Mandelbrot::new(
-                1024,
-                128,
-                128,
-                -2.3, 0.8,
-                -1.1, 1.1
-            );
-
-            b.iter(|| -> Mandelbrot {
-                let mut copy = mandelbrot.clone();
-                unsafe { copy.old_update_avx2() };
                 return copy;
             });
         }
